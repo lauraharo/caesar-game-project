1d0ee73437d7ce994d944a733026018589629c5e
lauraharo <laura.haro22@gmail.com>
2019-11-20 18:43:02 +0200

    Enemy placeholder added

---
+++ CaesarGame/Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private bool dirRight = true;
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        if (dirRight)
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        else
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+
+        if (transform.position.x >= -4.0f)
+        {
+            dirRight = false;
+        }
+
+        if (transform.position.x <= -6)
+        {
+            dirRight = true;
+        }
+    }
+}
