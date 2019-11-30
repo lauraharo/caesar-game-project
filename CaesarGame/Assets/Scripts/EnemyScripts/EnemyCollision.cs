b660ea712d31bcb83575fe03aa3d50969ea98d0c
lauraharo <laura.haro22@gmail.com>
2019-11-30 18:47:22 +0200

    HealthBar v1

---
+++ CaesarGame/Assets/Scripts/EnemyScripts/EnemyCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    [SerializeField] HealthBar healthBar;
+    private float size;
+
+    private void Start()
+    {
+        size = healthBar.transform.localScale.x * 10;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        size -= 1;
+        healthBar.SetSize(size);
+    }
+}
