18650ba3c4cee1d18e0d0a2278ed6040a4aa41bd
Hernesaho Miklos <miklos.hernesaho@kuumaict.fi>
2019-12-05 12:57:39 +0200

    Music loop fixed

---
+++ CaesarGame/Assets/Scripts/AudioScript/ThemeLoop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemeLoop : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.PlayOneShot(musicStart);
+        musicSource.PlayScheduled(AudioSettings.dspTime + musicStart.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
